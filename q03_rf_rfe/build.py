f8033b89cb6e74e699c3545ee18271757a35cea0
SL345 <sherilyn345@gmail.com>
2018-07-23 05:17:00 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,5 +1,7 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
+from collections import OrderedDict
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
@@ -8,4 +10,24 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(data):
+    X = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
+    names = data.columns
+    rfc = RandomForestClassifier()
+    rfc.fit(X,y)
+    i = int(len(X.columns)/2)
+    #stop the search when only the last feature is left
+    rfe = RFE(rfc,n_features_to_select= i  ,step=1)
+    rfe.fit(X,y)
+
+    d = OrderedDict(zip(names,rfe.ranking_))
+
+    top_features = []
+    for k,v in d.items():
+        if v == 1:
+            top_features.append(k)
+
+    return top_features
+
 
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,3 +1,4 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -8,3 +9,13 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(data):
+    np.random.seed(9)
+    X = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
+    rf_selector = SelectFromModel(RandomForestClassifier())
+    rf_selector.fit(X, y)
+    rf_support = rf_selector.get_support()
+    selected_variables = X.loc[:,rf_support].columns.tolist()
+    return selected_variables
+
